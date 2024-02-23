2dd2e95be3a740851e9492075deb701a2d7cab7d
AShmyrev <shmyrevandrey@gmail.com>
2024-02-23 03:34:55 +0300

    Added DisemvowelTrolls

---
+++ src/Solutions7kyu/DisemvowelTrolls/Runner.java
@@ -0,0 +1,7 @@
+package Solutions7kyu.DisemvowelTrolls;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(DisemvowelTrolls.removeVowels("This website is for losers LOL!"));
+    }
+}
