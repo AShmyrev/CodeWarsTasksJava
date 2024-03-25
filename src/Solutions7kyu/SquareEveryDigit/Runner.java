d5403c09a92977ddb82c3504a0ce90c4595e0cdc
AShmyrev <shmyrevandrey@gmail.com>
2024-03-25 18:13:21 +0300

    Changed RemoveStringSpaces and Added BasicMathematicalOperations in 8kyu; SquareEveryDigit in 7kyu

---
+++ src/Solutions7kyu/SquareEveryDigit/Runner.java
@@ -0,0 +1,7 @@
+package Solutions7kyu.SquareEveryDigit;
+
+public class Runner {
+    public static void main(String[] args) {
+        SquareEveryDigit.squareDigits(228);
+    }
+}
--- src/Solutions8kyu/RemoveStringSpaces/RemoveStringSpaces.java
+++ src/Solutions8kyu/RemoveStringSpaces/RemoveStringSpaces.java
@@ -2,9 +2,10 @@ package Solutions8kyu.RemoveStringSpaces;
 
 public class RemoveStringSpaces {
     public static String noSpace(final String x) {
-        return x.replaceAll(" ", "");
+        return x.replace(" ", "");
         /**
          * return x.replaceAll("\\s+","");
+         * current one uses regexp, the other doesn't, so it's more efficient
          * \s matches a space, tab, new line, carriage return, form feed or vertical tab.
          * + says one or more occurrences.
          */
